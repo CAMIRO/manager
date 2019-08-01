897c0af1da323bac6b1a1f0fd9af932d8a650e87
Juan Sarmiento <juansarmiento@Mac-mini-de-TerAdmin.local>
2019-08-01 10:20:47 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import Reducers from './reducers';
+
+class App extends Component {
+  componentWillMount() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyCkq5F89s6IB-cHwfPePTBgBk2Bg0jfoZ4',
+      authDomain: 'manager-11fbd.firebaseapp.com',
+      databaseURL: 'https://manager-11fbd.firebaseio.com',
+      projectId: 'manager-11fbd',
+      storageBucket: '',
+      messagingSenderId: '672756536073',
+      appId: '1:672756536073:web:16bbb5af13fef907',
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
+  render() {
+    return (
+      <Provider store={createStore(Reducers)}>
+        <View>
+          <Text>Hello!</Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+export default App;
